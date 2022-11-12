bbc498be93ae3ece4a8863833c813b1dcb60c020
Fredrik Alenius <Allennius>
2022-11-12 22:04:20 +0100

    Updates

---
+++ precache-manifest.54e4124fa826674b2581aea9263fa7e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c7b9732e413c35361c9",
+    "url": "/finnsatergard/static/css/main.b4ed8848.chunk.css"
+  },
+  {
+    "revision": "8c7b9732e413c35361c9",
+    "url": "/finnsatergard/static/js/main.aca2674a.chunk.js"
+  },
+  {
+    "revision": "aa1e0f9ca54fbc43935f",
+    "url": "/finnsatergard/static/js/runtime~main.cc5047d0.js"
+  },
+  {
+    "revision": "bb929a60571beee7ca24",
+    "url": "/finnsatergard/static/js/2.8c7b1906.chunk.js"
+  },
+  {
+    "revision": "e94b933a435eb14e88e4a0c25670676d",
+    "url": "/finnsatergard/static/media/mushroom.e94b933a.jpg"
+  },
+  {
+    "revision": "b57c0e55c49a68c6ce524986b356e0dc",
+    "url": "/finnsatergard/static/media/vintra.b57c0e55.jpg"
+  },
+  {
+    "revision": "80a7f30b580b270a04439171970f1b04",
+    "url": "/finnsatergard/static/media/roadtosun.80a7f30b.jpg"
+  },
+  {
+    "revision": "713afcab95f4ec587b970b284a137255",
+    "url": "/finnsatergard/static/media/sheep.713afcab.jpg"
+  },
+  {
+    "revision": "a9bc451005de337ce144d5a352582b67",
+    "url": "/finnsatergard/static/media/cow.a9bc4510.jpg"
+  },
+  {
+    "revision": "6dfcaf936487b3d3a79e3aa2c35fb061",
+    "url": "/finnsatergard/index.html"
+  }
+];
